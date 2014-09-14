155155950e1734bcb8a54b97b67eb4db9da000e3
Lowell Bander <lowellbander@gmail.com>
2014-09-14 15:54:51 -0700

    does this fix it?

--- web.js
+++ web.js
@@ -1,8 +1,8 @@
 var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
-var logger = require('morgan');
+var morgan = require('morgan');
 
-app.use(logger);
+app.use(morgan('combined'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
