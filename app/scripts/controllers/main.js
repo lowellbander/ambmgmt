91b5b2d4a0baca155b217b5048392b49da5be57e
Lowell Bander <lowellbander@gmail.com>
2014-09-14 15:03:35 -0700

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ambmgmtApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ambmgmtApp
+ */
+angular.module('ambmgmtApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
