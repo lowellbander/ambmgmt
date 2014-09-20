a9989ea246acc9eeb4bc46e1ae6941b964db2e47
Lowell Bander <lowellbander@gmail.com>
2014-09-20 15:26:03 -0700

    fixed build PhantomJs build error

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,6 +10,11 @@
 angular.module('ambmgmtApp')
   .controller('MainCtrl', ['$scope', 'Tour', 'Party',
     function ($scope, Tour, Party) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
 
     $scope.parties = Party.get();
 
